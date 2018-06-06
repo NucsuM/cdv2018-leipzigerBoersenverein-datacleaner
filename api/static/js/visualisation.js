39790e7eb48ea60e0291738f95aaf63d12ae4b99
mrwu <eswd@wustig.net>
2018-06-06 17:25:41 +0200

    add points to map added

--- api/static/js/visualisation.js
+++ api/static/js/visualisation.js
@@ -1,31 +1,21 @@
+// get cities coordinates from api
 function get_cities(){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",'http://localhost:5000/api',false);
-    Httpreq.send(null);
-    return Httpreq.responseText;          
+    var request = new XMLHttpRequest();
+    request.open("GET",'http://localhost:5000/api',false);
+    request.send(null);
+    return request.responseText;          
 }
 
-var json_obj = JSON.parse(get_cities());
-for (i in json_obj.cities){
-    console.log(json_obj.cities[i].lan)
-}
-
-
-
-//var test = get_cities()
-
-document.getElementById('json').innerHTML=json_obj.cities[i]
-
-/*
+//call and center map to Leipzig
 var mymap = L.map('mapid').setView([51.3391827 ,12.3810549], 11);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
 
-
-
-for (i in cities) {
+// set points on map
+var cities = JSON.parse(get_cities()).cities;
+for (i in cities){
     var lat = cities[i]['lat'];
     var lan = cities[i]['lan'];
     var rad = cities[i]['radius'];
@@ -37,19 +27,15 @@ for (i in cities) {
     }).addTo(mymap);
 }
 
-
-
-
-
+/*
+// set a cycle to a map
 var circle = L.circle([51.3391827, 12.3810549], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 3600
 }).addTo(mymap);
-
-circle.bindPopup("<b>Hello world!</b><br>I am a popup.");
 */
-
-
+// a popup
+//circle.bindPopup("<b>Hello world!</b><br>I am a popup.");
 
