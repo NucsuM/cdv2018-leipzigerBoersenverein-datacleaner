41b2d7ee5f280bbcdc2a2491995545667a6be48a
MrWu <eswd@wustig.de>
2018-06-11 14:04:07 +0200

    create csv with distinct cities and number of companies

--- api/summery.py
+++ api/summery.py
@@ -4,9 +4,10 @@ CSV_FILE = 'csv_new.csv'
 
 def create_city_list():
     """
-    Create a csv with all citys and the number of companies
+    Create a csv with all citys.
     """
-    city_list=[]
+
+    companies_per_city = {}
 
     with open (CSV_FILE, 'r') as f:
         csv_data = csv.DictReader(f)
@@ -14,28 +15,36 @@ def create_city_list():
 
         # generate city list
         for i in rows:
-            if i['Stadt'] not in city_list:
-                city_list.append(i['Stadt'])
+            city = i['Stadt']
+            if city not in companies_per_city:
+                companies_per_city[city] = 1
         
+        # count companies per city
+        for i in rows:
+            city = i['Stadt']
+            if city in companies_per_city.keys():
+                companies_per_city[city] += 1
 
-        #crate a new csv_file 
-        fieldnames = ['Stadt', 'Koordinaten', 'Anzahl Firmen']
+
+
+        #write result in new csv file 
+        fieldnames = ['Stadt','Anzahl_Firmen' ,'lat', 'lan']
 
         with open('city_list.csv', 'w') as new_f:
 
-            # generate file
+            # generate csv-file
             new_csv = csv.DictWriter(new_f, fieldnames=fieldnames)
             new_csv.writeheader()
 
-            # write cities
-            for i in city_list:
-                new_csv.writerow({'Stadt':i})
+            # write content
+            for i in companies_per_city.keys():
+                new_csv.writerow({'Stadt':i,'Anzahl_Firmen':companies_per_city[i]})
             
-            # write amount of companies
-
-            for i in rows:
-
 
 
 if __name__ == '__main__':
     create_city_list()
+
+
+
+
