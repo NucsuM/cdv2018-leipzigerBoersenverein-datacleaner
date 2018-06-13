e20235df329a74ae691656ea741bf9acbd4d6fd0
Nucsum <nucsum@mailbox.org>
2018-06-13 21:08:20 +0200

    small fixes

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -9,11 +9,6 @@
     <tbody>
     <tr v-for="(part, key) in splitedString" :key="key">
     <td>{{part}}</td>
-<!--     <td>
-      <select>
-      <option v-for="(clas, ckey) in classifications" :value="clas" :key="ckey">{{clas}}</option>
-      </select>
-    </td> -->
       <td>
       <button @click="addToClassification(part,clas)" v-for="(clas, ckey) in classifications" :value="clas" :key="ckey">{{clas}}</button>
     </td>
@@ -27,62 +22,66 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "HelloWorld",
   data() {
     return {
-      line:
-        "Hallwig, Hugo, Buchhandlung, Marburg",
-        lineIndex:1,
-        classifications:['Name','Inhaber','Titel','Stadt', 'Stadt (früher)'],
-        classificated: {},
-        classificatedMap: [],
-        errors:[]
+      line: "Hallwig, Hugo, Buchhandlung, Marburg",
+      lineIndex: 1,
+      classifications: ["Name", "Inhaber", "Titel", "Stadt", "Stadt (früher)"],
+      classificated: {},
+      classificatedMap: [],
+      errors: []
     };
   },
   computed: {
     splitedString: function() {
       /* eslint-disable */
-      this.classificated = {'Name': [], 'Inhaber':[], 'Titel':[], 'Stadt' :[], 'Stadt (früher)':[]}
-      this.classificatedMap = []
-      let splitedString = this.line.split(', ');
+      this.classificated = {
+        Name: [],
+        Inhaber: [],
+        Titel: [],
+        Stadt: [],
+        "Stadt (früher)": []
+      };
+      this.classificatedMap = [];
+      let splitedString = this.line.split(", ");
       return splitedString;
       /* eslint-enable */
     }
   },
   methods: {
-    addToClassification: function(part, clas){
-      console.dir(this)
-      if (this.classificatedMap.indexOf(part) === -1)
-      {
-        
-        this.classificated[clas].push(part)
-        this.classificatedMap.push(part)
+    addToClassification: function(part, clas) {
+      console.dir(this);
+      if (this.classificatedMap.indexOf(part) === -1) {
+        this.classificated[clas].push(part);
+        this.classificatedMap.push(part);
+      } else {
+        console.log("exists - not added");
       }
-      else {
-        console.log('exists - not added')
-      }
-
     },
     getNextLine: function() {
-        axios.get('http://localhost:5000/robert').then(response => {
-          this.line = response.data.file
-          })
-          .catch(e => {
-            this.errors.push(e)
-          })
+      axios
+        .get("http://localhost:5000/robert")
+        .then(response => {
+          this.line = response.data.file;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
     },
     sendToApi: function() {
-      axios.post('/save', this.classificated)
-        .then(function (response) {
+      axios
+        .post("http://localhost:5000/save", this.classificated)
+        .then(function(response) {
           console.log(response);
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
-      }
+    }
   }
 };
 </script>
